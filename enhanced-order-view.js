ed07a788515d0b9765ba9641aa050ad616764294
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-03-02 06:23:16 -0800

    added enhanced-order-view.js

---
+++ enhanced-order-view.js
@@ -0,0 +1,204 @@
+// ==UserScript==
+// @name         Enhance Order View
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  enhance order view
+// @author       Pei
+// @match        https://aftlite-na.amazon.com/wms/view_order/*
+// @match        https://aftlite-na.amazon.com/wms/view_order
+// @require      https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js
+// @grant        GM_addStyle
+// ==/UserScript==
+
+const SELECTOR = {
+    pickLists: '#picklists_table',
+    totesTable: 'body > table:nth-child(5) > tbody:nth-child(1) > tr:nth-child(12) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td'
+}
+let allSpoo = [];
+let allTracking = [];
+let allItemNo = [];
+
+let qrcode;
+
+(function () {
+
+    'use strict';
+
+    //grab tote info
+    let allTotes = document.querySelectorAll(SELECTOR.totesTable);
+
+    //categorize tote info, (Spoo, tracking, number of items)
+    allTotes.forEach(function (tote) {
+        let info = tote.innerText.split(/[\n:]+/);
+        allSpoo.push(info[0]);
+        allTracking.push(info[2]);
+        allItemNo.push(info[3]);
+    });
+
+    //clone the table of picklists
+    let myTable = document.querySelector(SELECTOR.pickLists).cloneNode(true);
+    myTable.setAttribute('id', 'myTable');
+
+
+    //manipulate myTable
+    //get rows
+    let rows = myTable.rows;
+
+    //remove useless attributes in th
+    for (let j = 0; j < rows[0].cells.length; j++) {
+        rows[0].cells[j].removeAttribute('width');
+        rows[0].cells[j].removeAttribute('align');
+    }
+
+    //modify some header text
+    rows[0].cells[0].textContent = 'Picklist';
+    rows[0].cells[1].textContent = 'Zone';
+    rows[0].cells[3].textContent = 'Totes';
+    rows[0].cells[4].textContent = 'Picker';
+
+    //remove and insert data
+    for (let i = 0; i < rows.length; i++) {
+
+        //remove unwanted columns
+        rows[i].deleteCell(6);
+        rows[i].deleteCell(5);
+
+        //add useful columns
+        let new_cell;
+        let spoo_index = allSpoo.indexOf(rows[i].cells[3].textContent.trim());
+        if (spoo_index === -1) {
+            //add Tracking code
+            new_cell = rows[i].insertCell(3);
+            new_cell.outerHTML = '<th>Tracking</th>'
+            //add number of items
+            new_cell = rows[i].insertCell(-1); //last column
+            new_cell.outerHTML = '<th></th>'
+        } else {
+            //add Tracking code
+            new_cell = rows[i].insertCell(3);
+            new_cell.innerHTML = allTracking[spoo_index].slice(0, -4) + '<b>' + allTracking[spoo_index].slice(-4) + '</b>';
+            new_cell.addEventListener('click', function () {
+                makeQRCode(allTracking[spoo_index]);
+            }, false);
+            //add number of items
+            new_cell = rows[i].insertCell(-1); //last column
+            new_cell.appendChild(document.createTextNode(allItemNo[spoo_index]));
+        }
+
+        //remove link from spoo number
+        if (i > 0) { //skip header
+            let spoo_cell = rows[i].cells[4];
+            let spoo_anchor_element = spoo_cell.firstElementChild;
+            let spoo_text_node = document.createTextNode(spoo_anchor_element.textContent);
+            spoo_anchor_element.textContent = '[open]';
+            spoo_anchor_element.style.marginLeft = '5px';
+            spoo_cell.insertBefore(spoo_text_node, spoo_anchor_element);
+
+            //add click to generate qrcode function
+            spoo_cell.addEventListener('click', function () {
+                makeQRCode(spoo_text_node.nodeValue);
+            }, false);
+        }
+    }
+
+    //create a container for new elements
+    let container = document.createElement('div');
+    container.setAttribute('id', 'new_container');
+
+    //create a container for qrcode
+    let qr = document.createElement('div');
+    qr.setAttribute('id', 'qrcode_container');
+
+    //create a div showing qr_text and append to qrcode_container
+    let qrText = document.createElement('div');
+    qrText.setAttribute('id', 'qr_text');
+
+    let qrcodeWithText = document.createElement('div');
+    qrcodeWithText.appendChild(qr);
+    //qrcodeWithText.appendChild(document.createElement('br'));
+    qrcodeWithText.appendChild(qrText);
+
+    //put new elements into the container
+    container.appendChild(myTable);
+    container.appendChild(qrcodeWithText);
+
+    //put whole container into body
+    let b = document.body;
+    b.insertBefore(document.createElement('br'), b.childNodes[4]);
+    b.insertBefore(document.createElement('br'), b.childNodes[4]);
+    b.insertBefore(container, b.childNodes[4]);
+
+}());
+
+
+function makeQRCode(str) {
+    //console.log('make code: ', str);
+    if (qrcode === undefined) {
+        qrcode = new QRCode(document.getElementById("qrcode_container"), {
+            text: str,
+            width: 250,
+            height: 250,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    } else {
+        qrcode.makeCode(str);
+    }
+    document.getElementById('qr_text').innerText = str;
+}
+
+
+//--------------------------- CSS -------------------------------------------------------//
+GM_addStyle(`
+    html, body {
+        margin-left:10px;
+    }
+
+    #myTable {
+        border-collapse: collapse;
+        font-family: monospace;
+        font-size: 18px;
+        min-width: 1100px;
+        max-width: calc(100vw - 600px);
+        margin-right: 100px;
+        flex: 1;
+    }
+
+    #myTable tr:nth-child(odd) {
+        background-color: whitesmoke;
+    }
+
+    #myTable tr:hover {
+        background-color: lightgrey;
+    }
+
+    #myTable tr th {
+        background-color: whitesmoke;
+        padding: 12px;
+        text-align: center;
+    }
+
+    #myTable tr td {
+        padding: 12px;
+     }
+
+    .last-row {
+    height: auto;
+    }
+
+    #new_container {
+        display: flex;
+        flex-flow: row wrap;
+        align-items: flex-start;
+    }
+
+    #qrcode_container {
+        flex: 1;
+    }
+
+    #qr_text {
+        margin-bottom: 5px;
+    }
+
+`);
