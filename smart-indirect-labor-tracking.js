c5d6fcdf18c900782a362e4c824c7c973040bb23
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-08-30 22:00:38 -0700

    [refactor] avoid eslint no-promise-executor-return

--- smart-indirect-labor-tracking.js
+++ smart-indirect-labor-tracking.js
@@ -49,8 +49,9 @@
 })();
 
 function wait(ms) {
-  // eslint-disable-next-line no-promise-executor-return
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 async function fetchUserHistoryPage(login) {
