ff9ac3ed36ac9c31a97ff15eb1fd72fbb63a957c
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-10-13 10:44:21 -0700

    refactor into one function

--- src/smart-labor-track.js
+++ src/smart-labor-track.js
@@ -1,87 +1,51 @@
 // ==UserScript==
 // @name         Smart Labor Track
 // @namespace    https://github.com/ethanhong/amzn-script
-// @version      2.0
+// @version      2.1
 // @description  Sign in to OBindirect according to current action
 // @author       Pei
 // @match        https://aftlite-na.amazon.com/indirect_action/signin_indirect_action*
 // ==/UserScript==
 
-let targetActivity = '';
-let skipList = [];
-let checkPeriod = 5; // minutes
-let delayAfterBRK = 10; // minutes
-
 // eslint-disable-next-line no-unused-vars
-function smartLaborTrack(activity, skip, period, brkDelay) {
-  targetActivity = activity;
-  skipList = [targetActivity, ...skip];
-  checkPeriod = period; // minutes
-  delayAfterBRK = brkDelay; // minutes
-
-  fetchUserHistoryPage()
-    .then((html) => getCurrentActivity(html))
-    .then((currentActivity) => nextAction(currentActivity))
-    .catch(() => console.error('[Smart Labot Tracking] Fail!'));
-}
-
-function isNASite() {
-  return window.location.hostname === 'aftlite-na.amazon.com';
-}
-
-function getLogin() {
-  return document.getElementsByTagName('span')[0].innerHTML.match(/\(([^)]+)\)/)[1];
-}
-
-function wait(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-async function fetchUserHistoryPage() {
-  console.log('func start: fetchUserHistoryPage');
+async function smartLaborTrack(targetActivity, skipList, checkPeriod = 5, delayAfterBRK = 10) {
+  const isNASite = window.location.hostname === 'aftlite-na.amazon.com';
+  const name = isNASite ? document.getElementsByTagName('span')[0].innerHTML.match(/\(([^)]+)\)/)[1] : 'hongph';
   const url = '/labor_tracking/lookup_history?user_name=';
-  const user = getLogin();
-  const res = await fetch(`${url}${encodeURIComponent(user)}`);
-  const txt = await res.text();
-  return new DOMParser().parseFromString(txt, 'text/html');
-}
 
-function getCurrentActivity(html) {
-  console.log('func start: getLatestAction');
-  const selector = isNASite()
-    ? '.reportLayout > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)'
-    : 'table.a-bordered > tbody > tr:nth-child(2) > td:nth-child(2) > p:nth-child(1)';
-  return html.querySelector(selector).textContent.trim();
-}
+  const response = await fetch(`${url}${name}`);
+  const text = await response.text();
+  const page = new DOMParser().parseFromString(text, 'text/html');
+  const currentAction = page
+    .querySelector(
+      isNASite
+        ? '.reportLayout > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)'
+        : 'table.a-bordered > tbody > tr:nth-child(2) > td:nth-child(2) > p:nth-child(1)'
+    )
+    .textContent.trim();
 
-async function nextAction(lastActivity) {
-  if (skipList.includes(lastActivity)) {
-    // do nothing
-    console.log(`latest action is ${lastActivity}. Wait 3 minust to check again.`);
-    await wait(checkPeriod * 60 * 1000);
-    checkin(''); // fake checkin just for page reload
-    return false;
+  if (skipList.includes(currentAction)) {
+    // do fake checkin after checkPeriod minutes to trigger reloading page
+    console.log(`latest action is ${currentAction}. Wait ${checkPeriod} minust to check again.`);
+    setTimeout(() => checkin(name, ''), checkPeriod * 1000);
+    // setTimeout(() => checkin(name, ''), checkPeriod * 60 * 1000);
+    return;
   }
-
-  if (lastActivity === 'BRK') {
-    console.log(`latest action is ${lastActivity}. Wait 10 minutes to checkin.`);
-    // checkin after 10 minutes
-    await wait(delayAfterBRK * 60 * 1000);
-    checkin(targetActivity);
-    return false;
+  if (currentAction === 'BRK') {
+    // checkin after delayAfterBRK minutes
+    console.log(`latest action is ${currentAction}. Wait ${delayAfterBRK} minutes to checkin.`);
+    setTimeout(() => checkin(name, targetActivity), delayAfterBRK * 1000);
+    // setTimeout(() => checkin(name, targetActivity), delayAfterBRK * 60 * 1000);
+    return;
   }
-
-  console.log(`latest action is ${lastActivity}. Checkin now.`);
-  checkin(targetActivity);
-  return false;
+  // checkin immediately
+  console.log(`latest action is ${currentAction}. Checkin now.`);
+  checkin(name, targetActivity);
 }
 
-function checkin(activity) {
-  const name = getLogin();
-  console.log(`${name} is checked in to ${activity}`);
-  document.getElementsByName('name')[0].value = name;
+function checkin(user, activity) {
+  console.log(`${user} is checked in to ${activity}`);
+  document.getElementsByName('name')[0].value = user;
   document.getElementsByName('code')[0].value = activity;
   document.getElementsByTagName('form')[0].submit();
 }
