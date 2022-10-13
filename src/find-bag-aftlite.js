2e6854c0a8a2b98b52429d6fee377a3cb2f160aa
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-10-12 20:42:07 -0700

    [refactor] change a lot

--- src/find-bag-aftlite.js
+++ src/find-bag-aftlite.js
@@ -17,7 +17,7 @@
 
 const e = React.createElement;
 
-const getCSS = (isAftlitePortal) => {
+function getCSS(isAftlitePortal) {
   const styleNA = `
     :root,
     body,
@@ -145,20 +145,22 @@ const getCSS = (isAftlitePortal) => {
     }
   `;
   return isAftlitePortal ? stylePortal : styleNA;
-};
+}
 
-const getActions = (table) => {
-  const actionRows = [...table.querySelectorAll('tbody > tr')];
-  actionRows.shift(); // remove header
-  return actionRows.map((tr) => [...tr.querySelectorAll('td')].map((td) => td.textContent.trim()));
-};
+function getActions(table) {
+  const [, ...actionRows] = [...table.querySelectorAll('tbody > tr')]; // exclude header
+  const getTdTextContents = (tr) => [...tr.querySelectorAll('td')].map((td) => td.textContent.trim());
+  return actionRows.map(getTdTextContents);
+}
 
-const TableHeader = ({ titles }) => {
-  const tableHeaders = titles.map((title) => e('th', { className: 'a-text-center', key: title }, title));
+function TableHeader({ titles }) {
+  const getHeaderComp = (t) => e('th', { className: 'a-text-center', key: t }, t);
+  const tableHeaders = titles.map(getHeaderComp);
   return e('tr', null, tableHeaders);
-};
+}
 
-const getPackageInfo = async (pickListId, isAftlitePortal) => {
+async function getPackageInfo(pickListId, isAftlitePortal) {
+  // url and selectors
   const fetchURL = isAftlitePortal
     ? '/picklist/view_picklist_history?picklist_id='
     : '/wms/view_picklist_history?picklist_id=';
@@ -166,167 +168,164 @@ const getPackageInfo = async (pickListId, isAftlitePortal) => {
   const completionTimeSelector = isAftlitePortal ? 'div.a-row:nth-child(10)' : 'tr:nth-child(6)';
   const cptSelector = isAftlitePortal ? 'div.a-row:nth-child(12)' : 'tr:nth-child(8)';
   const orderIdSelector = isAftlitePortal ? 'div.a-row:nth-child(2)' : 'tr:nth-child(2)';
-
+  // define re
   const timeRe = /\d{1,2}:\d{1,2}/;
   const statusRe = /\(([\w-]+)\)/;
   const orderIdRe = /\d{7}/;
 
-  return fetch(`${fetchURL}${pickListId}`)
-    .then((res) => res.text())
-    .then((page) => {
-      const packageInfo = Array(4);
-      const html = new DOMParser().parseFromString(page, 'text/html');
+  try {
+    const response = await fetch(`${fetchURL}${pickListId}`);
+    const text = await response.text();
+    const html = new DOMParser().parseFromString(text, 'text/html');
+    const packageInfo = [];
 
-      // extract completion time
-      const completionTime = html.querySelector(completionTimeSelector).textContent.match(timeRe);
-      packageInfo[0] = completionTime ? completionTime[0] : '-';
-      // extract CPT
-      const cpt = html.querySelector(cptSelector).textContent.match(timeRe);
-      packageInfo[1] = cpt ? cpt[0] : '-';
-      // extract status
-      const status = html.querySelector(statusSelector).textContent.match(statusRe);
-      packageInfo[2] = status ? status[1] : '-';
-      // extract orderId
-      const orderId = html.querySelector(orderIdSelector).textContent.match(orderIdRe);
-      packageInfo[3] = orderId ? orderId[0] : '-';
-
-      return packageInfo;
-    })
-    .catch((err) => {
-      console.log('[getPackgeInfo]Fetch error: ', err);
-      return Array(4).fill('-');
-    });
-};
-
-const getTimeStyle = (timeStamp, cpt, currentTime) => {
-  if (!timeStamp || !cpt) {
-    return '';
+    // extract completion time
+    const completionTime = html.querySelector(completionTimeSelector).textContent.match(timeRe);
+    packageInfo[0] = completionTime ? completionTime[0] : '-';
+    // extract CPT
+    const cpt = html.querySelector(cptSelector).textContent.match(timeRe);
+    packageInfo[1] = cpt ? cpt[0] : '-';
+    // extract status
+    const status = html.querySelector(statusSelector).textContent.match(statusRe);
+    packageInfo[2] = status ? status[1] : '-';
+    // extract orderId
+    const orderId = html.querySelector(orderIdSelector).textContent.match(orderIdRe);
+    packageInfo[3] = orderId ? orderId[0] : '-';
+    return packageInfo;
+  } catch (err) {
+    console.log('[getPackgeInfo]Fetch error: ', err);
+    return Array(4).fill('-');
   }
+}
+
+function getTimeStyle(timeStamp, cpt, currentTime) {
+  if (!timeStamp || !cpt) return '';
 
   const lateWindow = (currentTime.getHours() + 1) % 24;
   const currentWindow = (currentTime.getHours() + 2) % 24;
   const nextWindow = (currentTime.getHours() + 3) % 24;
-  const startHour = parseInt(cpt.split(':')[0], 10);
-  if (startHour === lateWindow) {
-    return 'late-window';
-  }
-  if (startHour === currentWindow) {
-    return 'current-window';
-  }
-  if (startHour === nextWindow) {
-    return 'next-window';
-  }
-  return '';
-};
 
-const ActionRow = ({ action, isFirstPackage, isAftlitePortal }) => {
+  const startHour = parseInt(cpt.split(':')[0], 10);
+  if (startHour === lateWindow) return 'late-window';
+  if (startHour === currentWindow) return 'current-window';
+  if (startHour === nextWindow) return 'next-window';
+  return '';
+}
+
+function ActionRow({ action, isFirstPackage, isAftlitePortal }) {
   const orderviewUrl = isAftlitePortal ? '/orders/view_order?id=' : '/wms/view_order/';
   const picklistUrl = isAftlitePortal
     ? '/picklist/pack_by_picklist?picklist_id='
     : '/wms/pack_by_picklist?picklist_id=';
 
-  const newAction = action.map((ele, j) => {
-    if (j === 3) return e('span', { className: 'monospace' }, ele);
-    if (j === 8)
+  const newAction = action.map((ele, i) => {
+    if (i === 3) return e('span', { className: 'monospace' }, ele);
+    if (i === 8)
       return e('div', null, [e('span', { className: 'spoo-dot' }), e('span', { className: 'monospace' }, ele)]);
-    if (j === 9) return e('a', { href: `${orderviewUrl}${ele}` }, ele);
-    if (j === 10) return e('a', { href: `${picklistUrl}${ele}` }, ele);
+    if (i === 9) return e('a', { href: `${orderviewUrl}${ele}` }, ele);
+    if (i === 10) return e('a', { href: `${picklistUrl}${ele}` }, ele);
     return ele;
   });
 
   const psolveStyle = newAction[5] === 'problem-solve' ? 'p-solve' : '';
   const timeWindowStyle = getTimeStyle(newAction[0], newAction[7], new Date());
   const topBorderStyle = isFirstPackage ? 'table-top-border' : '';
-
   const cells = newAction.map((cell, index) => e('td', { className: 'a-text-center', key: index }, cell));
-
   return e('tr', { className: `${psolveStyle} ${timeWindowStyle} ${topBorderStyle}` }, cells);
-};
+}
 
-const mapToNewAction = (action, isAftlitePortal) => {
+function mapToNewAction(isAftlitePortal) {
   // convert old table data in to new table
-  const newAction = [];
-  if (isAftlitePortal) {
-    newAction[0] = action[0];
-    newAction[1] = action[1];
-    newAction[2] = action[2];
-    newAction[3] = action[3];
-    newAction[4] = action[4];
-    newAction[5] = ''; // status
-    newAction[6] = ''; // completion time
-    newAction[7] = ''; // cpt
-    newAction[8] = action[9];
-    newAction[9] = ''; // orderId
-    newAction[10] = action[12];
-    newAction[11] = action[11];
-  } else {
-    newAction[0] = action[0];
-    newAction[1] = action[1];
-    newAction[2] = action[2];
-    newAction[3] = action[3];
-    newAction[4] = action[4];
-    newAction[5] = ''; // status
-    newAction[6] = ''; // completion time
-    newAction[7] = ''; // cpt
-    newAction[8] = action[10];
-    newAction[9] = ''; // orderId
-    newAction[10] = action[13];
-    newAction[11] = action[12];
-  }
-  return newAction;
-};
+  return (action) => {
+    const newAction = [];
+    if (isAftlitePortal) {
+      newAction[0] = action[0];
+      newAction[1] = action[1];
+      newAction[2] = action[2];
+      newAction[3] = action[3];
+      newAction[4] = action[4];
+      newAction[5] = ''; // status
+      newAction[6] = ''; // completion time
+      newAction[7] = ''; // cpt
+      newAction[8] = action[9];
+      newAction[9] = ''; // orderId
+      newAction[10] = action[12];
+      newAction[11] = action[11];
+    } else {
+      newAction[0] = action[0];
+      newAction[1] = action[1];
+      newAction[2] = action[2];
+      newAction[3] = action[3];
+      newAction[4] = action[4];
+      newAction[5] = ''; // status
+      newAction[6] = ''; // completion time
+      newAction[7] = ''; // cpt
+      newAction[8] = action[10];
+      newAction[9] = ''; // orderId
+      newAction[10] = action[13];
+      newAction[11] = action[12];
+    }
+    return newAction;
+  };
+}
 
-const doRecursiveFetch = async (urlArray, spooArray, startIndex, setProgress) => {
-  const url = urlArray[startIndex];
-  const spoo = spooArray[startIndex];
-  if (!url || !spoo) return [];
+async function doRecursiveFetch(urlAndspoos, startIndex, setProgress) {
+  if (!urlAndspoos[startIndex]) return [];
+  const [url, spoo] = urlAndspoos[startIndex] ? urlAndspoos[startIndex] : [];
+  // start fetching
+  const response = await fetch(url);
+  const text = await response.text();
+  const [currResult] = text.slice(text.indexOf(spoo) + 20, text.indexOf(spoo) + 80).match(/[\w\d]{20}/) || [];
+  // set progress
+  const fetchPercentage = ((startIndex + 1) / urlAndspoos.length) * 100;
+  setProgress(fetchPercentage % 100 ? fetchPercentage.toFixed(1) : fetchPercentage); // no decimal point when 0 and 100
 
-  const fetchPercentage = (startIndex / urlArray.length) * 100;
-  setProgress(fetchPercentage ? fetchPercentage.toFixed(1) : 0);
-  let currResult;
-  await fetch(url)
-    .then((res) => res.text())
-    .then((res) => {
-      [currResult] = res.slice(res.indexOf(spoo) + 20).match(/[\w\d]{20}/);
-    });
+  return [currResult, ...(await doRecursiveFetch(urlAndspoos, startIndex + 1, setProgress))];
+}
 
-  return [currResult, ...(await doRecursiveFetch(urlArray, spooArray, startIndex + 1, setProgress))];
-};
-
-const fetchTrackCode = async (searchTerm, setProgress, isAftlitePortal) => {
-  if (!searchTerm) return [];
-  const allActions = getActions(document.querySelector('#main-table'));
-  const [targetAction] = allActions.filter((a) => a[8] === searchTerm);
-  const relatedActions = allActions
-    .filter((a) => a[6] === targetAction[6] && a[7] === targetAction[7]) // same completion time && same cpt
-    .filter((a) => !(a[8] === targetAction[8])); // excludes target itself
-
-  if (relatedActions.length === 0) {
-    alert("Can't find any related bag.");
-    return [];
-  }
-
+async function fetchTrackCode(actionToFetch, setProgress, isAftlitePortal) {
   const orderUrl = isAftlitePortal ? '/orders/view_order?id=' : '/wms/view_order/';
-  const urls = relatedActions.map((a) => `${orderUrl}${a[9]}`);
-  const spoos = relatedActions.map((a) => a[8]);
-  const codes = await doRecursiveFetch(urls, spoos, 0, setProgress);
-  setProgress(100);
-  return codes;
-};
+  const getUrlAndSpoos = (action) => [`${orderUrl}${action[9]}`, action[8]];
+  const urlAndspoos = actionToFetch.map(getUrlAndSpoos);
 
-const SearchBar = ({ isAftlitePortal }) => {
+  const codes = await doRecursiveFetch(urlAndspoos, 0, setProgress);
+  return codes.filter((x) => Boolean(x)); // remove empty, ex: problem bags
+}
+
+function getRelatedActions(searchTerm) {
+  const allActions = getActions(document.querySelector('#main-table'));
+
+  const isSearchTarget = (action) => action[8] === searchTerm;
+  const [targetAction] = allActions.filter(isSearchTarget);
+
+  const isRelated = (action) => action[6] === targetAction[6] && action[7] === targetAction[7]; // same completion time && same cpt
+  const isNotSearchTarget = (action) => !isSearchTarget(action);
+  const relatedActions = allActions.filter(isRelated).filter(isNotSearchTarget); // excludes target itself
+
+  return [targetAction, ...relatedActions];
+}
+
+function SearchBar({ isAftlitePortal }) {
   const [searchTerm, setSearchTerm] = React.useState('');
   const [progress, setProgress] = React.useState(0);
 
   const handleOnChange = (evt) => setSearchTerm(evt.target.value);
 
   const handleOnClick = async () => {
-    const codes = await fetchTrackCode(searchTerm, setProgress, isAftlitePortal);
-    if (!codes) return;
-    setTimeout(() => {
-      prompt('Copy and paste to the search bar in COMO package tab.', codes);
-      setProgress(0);
-    }, 500);
+    if (!searchTerm) return;
+    const actionToFetch = getRelatedActions(searchTerm);
+    const scannableId = await fetchTrackCode(actionToFetch, setProgress, isAftlitePortal);
+    if (scannableId.length <= 1) {
+      alert('No related bags found.');
+    } else {
+      setTimeout(() => {
+        prompt(
+          `Found ${scannableId.length - 1} related bags.\nCopy and paste the relust into COMO for locations.`,
+          scannableId
+        );
+        setProgress(0);
+      }, 100);
+    }
   };
 
   const searchForm = e('form', { id: 'search-form' }, [
@@ -348,9 +347,9 @@ const SearchBar = ({ isAftlitePortal }) => {
   ]);
 
   return e('div', { id: 'search-bar' }, [searchForm]);
-};
+}
 
-const MainTable = ({ oldTable, isAftlitePortal }) => {
+function MainTable({ oldTable, isAftlitePortal }) {
   const titles = [
     'Timestamp',
     'Action',
@@ -366,45 +365,39 @@ const MainTable = ({ oldTable, isAftlitePortal }) => {
     'User',
   ];
 
-  const isPack = (action) => action[1] === 'pack' && action[2] === 'pack';
+  const isPack = (action) => action[1] === 'pack';
   const isIndirect = (action) => action[2] === 'indirect';
-  const isUniqueSpoo = (action, i, allActions) => {
-    const spooCell = isAftlitePortal ? action[9] : action[10];
-    const allSpoos = allActions.map((act) => (isAftlitePortal ? act[9] : act[10]));
-    if (!spooCell) return true;
-    return allSpoos.indexOf(spooCell) === i;
+  const isLatestSpoo = (_, i, allActions) => {
+    const getSpooCell = (act) => (isAftlitePortal ? act[9] : act[10]);
+    const allSpoos = allActions.map(getSpooCell);
+    const spooCell = allSpoos[i];
+    return spooCell ? allSpoos.indexOf(spooCell) === i : true;
   };
 
   const [newActions, setNewActions] = React.useState(
     getActions(oldTable)
       .filter((action) => isPack(action) || isIndirect(action))
-      .filter((action, i, allActions) => isUniqueSpoo(action, i, allActions))
-      .map((action) => mapToNewAction(action, isAftlitePortal))
+      .filter(isLatestSpoo)
+      .map(mapToNewAction(isAftlitePortal))
   );
 
   const header = e(TableHeader, { titles, key: 'main-table-header' });
   const rows = newActions.map((action, i, allActions) => {
-    const isFirstPackage = i === 0 || allActions[i][6] !== allActions[i - 1][6];
+    const isFirstPackage = !i || allActions[i][6] !== allActions[i - 1][6];
     return e(ActionRow, { action, isFirstPackage, isAftlitePortal, key: action[8] });
   });
 
   React.useEffect(() => {
-    newActions.map((v, i) => {
-      const pickListId = v[10];
-      if (!pickListId) return null;
-      getPackageInfo(pickListId, isAftlitePortal).then((packageInfo) => {
-        setNewActions((prev) =>
-          prev.map((w, j) => {
-            if (j === i) {
-              const newValue = w.slice();
-              [newValue[6], newValue[7], newValue[5], newValue[9]] = packageInfo;
-              return newValue;
-            }
-            return w;
-          })
-        );
-      });
-      return null;
+    newActions.map(async (action, i) => {
+      const { 10: pickListId } = action;
+      if (!pickListId) return;
+      const packageInfo = await getPackageInfo(pickListId, isAftlitePortal);
+      const fetchedActions = (prevState) => {
+        const currAction = [...prevState[i]];
+        [currAction[6], currAction[7], currAction[5], currAction[9]] = packageInfo;
+        return [...prevState.slice(0, i), currAction, ...prevState.slice(i + 1)];
+      };
+      setNewActions(fetchedActions);
     });
   }, []);
 
@@ -415,21 +408,20 @@ const MainTable = ({ oldTable, isAftlitePortal }) => {
     e('tbody', null, [header, ...rows])
   );
   return e('div', null, [searchBar, newTable]);
-};
+}
 
-const TableSwitch = ({ isOriginalTable, setIsOriginalTable }) =>
-  e(
-    'form',
-    null,
+function TableSwitch({ isOriginalTable, setIsOriginalTable }) {
+  return e('form', null, [
     e('input', {
       type: 'checkbox',
       checked: isOriginalTable,
       onChange: () => setIsOriginalTable((prev) => !prev),
     }),
-    ' Show original table'
-  );
+    ' Show original table',
+  ]);
+}
 
-const App = ({ oldTable, isAftlitePortal }) => {
+function App({ oldTable, isAftlitePortal }) {
   const [isOriginalTable, setIsOriginalTable] = React.useState(false);
   const mainTable = e(MainTable, { oldTable, isAftlitePortal, key: 'main-table' });
   const tableSwitch = e(TableSwitch, { isOriginalTable, setIsOriginalTable, key: 'table-switch' });
@@ -442,10 +434,10 @@ const App = ({ oldTable, isAftlitePortal }) => {
   }, [isOriginalTable]);
 
   return e(React.Fragment, null, [tableSwitch, mainTable]);
-};
+}
 
 // eslint-disable-next-line no-unused-vars
-const startBagFinder = () => {
+function startBagFinder() {
   const isAftlitePortal = window.location.hostname === 'aftlite-portal.amazon.com';
 
   // add stylesheet
@@ -463,4 +455,4 @@ const startBagFinder = () => {
   const oldTable = document.querySelector('#main-content > table');
   oldTable.before(rootDiv);
   ReactDOM.createRoot(rootDiv).render(e(App, { oldTable, isAftlitePortal }));
-};
+}
