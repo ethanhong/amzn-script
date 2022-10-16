7fde6728a680e1b9cfb531314e6a10a815b5e320
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-10-16 15:27:56 -0700

    update script meta data

--- src/smart-labor-track.js
+++ src/smart-labor-track.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Smart Labor Track
-// @namespace    https://github.com/ethanhong/amzntools
-// @version      2.1
+// @namespace    https://github.com/ethanhong/amzntools-src/tree/release
+// @version      2.1.1
 // @description  Sign in to OBindirect according to current action
 // @author       Pei
 // @match        https://aftlite-na.amazon.com/indirect_action/signin_indirect_action*
 // @match        https://aftlite-portal.amazon.com/indirect_action*
-// @require      file:///C:/Users/pyhon/git/ethanhong/amzntools-src/src/smart-labor-track.js
+// @updateURL    https://ethanhong.github.io/amzntools-src/smart-labor-track.user.js
+// @downloadURL  https://ethanhong.github.io/amzntools-src/smart-labor-track.user.js
+// @supportURL   https://github.com/ethanhong/amzntools-src/issues
 // ==/UserScript==
 
 // eslint-disable-next-line no-unused-vars
