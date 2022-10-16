bd35f45d4c4dfb4d66345b8c842f65eebc0ca0ea
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-10-16 10:31:10 -0700

    rewrite code

--- src/clock.js
+++ src/clock.js
@@ -1,63 +1,52 @@
 // ==UserScript==
 // @name         Clock
 // @namespace    https://github.com/ethanhong/amzn-script
-// @version      1.0
+// @version      1.0.1
 // @description  clock
 // @author       Pei
 // @match        https://aftlite-portal.amazon.com/*
 // @match        https://aftlite-na.amazon.com/*
 // @match        https://como-operations-dashboard-iad.iad.proxy.amazon.com/*
+// @grant        GM_addStyle
 // ==/UserScript==
 
 // eslint-disable-next-line no-unused-vars
-function clock() {
-  const clockDiv = document.createElement('div');
-  clockDiv.setAttribute('id', 'clock');
-  document.body.append(clockDiv);
-  addCSS();
-  startTime();
-}
+;(function clock() {
+  const clockDiv = document.createElement('div')
+  clockDiv.setAttribute('id', 'clock')
+  document.body.append(clockDiv)
+  startTime()
+})()
 
 function startTime() {
-  const today = new Date();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  let s = today.getSeconds();
-  h = checkTime(h);
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('clock').innerHTML = `${h}:${m}:${s}`;
-  setTimeout(startTime, 1000);
+  const checkTime = (x) => (x < 10 ? `0${x}` : x)
+  const today = new Date()
+  let h = today.getHours()
+  let m = today.getMinutes()
+  let s = today.getSeconds()
+  h = checkTime(h)
+  m = checkTime(m)
+  s = checkTime(s)
+  document.getElementById('clock').innerHTML = `${h}:${m}:${s}`
+  setTimeout(startTime, 1000)
 }
 
-function checkTime(i) {
-  if (i < 10) {
-    return `0${i}`;
-  } // add zero in front of numbers < 10
-  return i;
-}
-
-function addCSS() {
-  const styles = `
-    div#clock {
-      width: 160px;
-      height: 48px;
-      line-height: 48px;
-      font-size: 32px;
-      color: #ccc;
-      text-align: center;
-      background-color: rgb(60, 60, 60, 90%);
-      border: 2px solid rgb(60, 60, 60);;
-      border-radius: 5px;
-      position: fixed;
-      bottom: 1%;
-      left: 50%;
-      margin-left: -80px;
-      z-index: 99;
-    }
-  `;
-
-  const styleSheet = document.createElement('style');
-  styleSheet.innerText = styles;
-  document.head.appendChild(styleSheet);
-}
+// eslint-disable-next-line no-undef
+GM_addStyle(`
+  div#clock {
+    width: 160px;
+    height: 48px;
+    line-height: 48px;
+    font-size: 32px;
+    color: #ccc;
+    text-align: center;
+    background-color: rgb(60, 60, 60, 90%);
+    border: 2px solid rgb(60, 60, 60);;
+    border-radius: 5px;
+    position: fixed;
+    bottom: 1%;
+    left: 50%;
+    margin-left: -80px;
+    z-index: 99;
+  }
+`)
