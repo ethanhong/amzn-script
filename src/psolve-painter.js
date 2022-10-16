2717badb675ed3b0c5d2dfad9dd350003fa5c137
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-10-16 16:09:12 -0700

    remove semicolon

--- src/psolve-painter.js
+++ src/psolve-painter.js
@@ -9,29 +9,29 @@
 
 // eslint-disable-next-line no-unused-vars
 function psolvePainter() {
-  const deliveryDateSelector = '#wms_table_dwell_time > tbody:nth-child(2) > tr > td:nth-child(10)';
+  const deliveryDateSelector = '#wms_table_dwell_time > tbody:nth-child(2) > tr > td:nth-child(10)'
 
   // calulate window hour
-  const currentTime = new Date();
-  const lateWindow = (currentTime.getHours() + 1) % 12 || 12;
-  const currentWindow = (currentTime.getHours() + 2) % 12 || 12;
-  const nextWindow = (currentTime.getHours() + 3) % 12 || 12;
+  const currentTime = new Date()
+  const lateWindow = (currentTime.getHours() + 1) % 12 || 12
+  const currentWindow = (currentTime.getHours() + 2) % 12 || 12
+  const nextWindow = (currentTime.getHours() + 3) % 12 || 12
 
-  document.querySelectorAll(deliveryDateSelector).forEach(cell => {
-    let [startHour] = cell.textContent.match(/\d{1,2}(?=:00)/);
-    startHour = parseInt(startHour, 10);
+  document.querySelectorAll(deliveryDateSelector).forEach((cell) => {
+    let [startHour] = cell.textContent.match(/\d{1,2}(?=:00)/)
+    startHour = parseInt(startHour, 10)
     if (startHour === lateWindow) {
-      cell.classList.add('late-window');
+      cell.classList.add('late-window')
     } else if (startHour === currentWindow) {
-      cell.classList.add('current-window');
+      cell.classList.add('current-window')
     } else if (startHour === nextWindow) {
-      cell.classList.add('next-window');
+      cell.classList.add('next-window')
     } else {
-      cell.classList.add('future-window');
+      cell.classList.add('future-window')
     }
-  });
+  })
 
-  addCSS();
+  addCSS()
 }
 
 function addCSS() {
@@ -48,8 +48,8 @@ function addCSS() {
     .future-window {
       background-color: LightGrey;
     }
-  `;
-  const styleSheet = document.createElement('style');
-  styleSheet.innerText = styles;
-  document.head.appendChild(styleSheet);
+  `
+  const styleSheet = document.createElement('style')
+  styleSheet.innerText = styles
+  document.head.appendChild(styleSheet)
 }
