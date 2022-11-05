c93c1877af2b687e13f32f377a918c1cd8de9d2a
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-11-04 17:19:05 -0700

    add test scripts

---
+++ dev/bob1.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         bob2
+// @namespace    https://github.com/ethanhong/amzn-tools/tree/main/release
+// @version      1.0.0
+// @description  A better outbound dashboard
+// @author       Pei
+// @match        https://aftlite-portal.amazon.com/ojs/OrchaJSFaaSTCoreProcess/OutboundDashboard
+// @match        https://aftlite-na.amazon.com/OutboundDashboard
+// @supportURL   https://github.com/ethanhong/amzn-tools/issues
+// ==/UserScript==
+
+const TIME_TH = '#cpt_table > thead > tr > th'
+const DASHBOARD_TR = '#cpt_table > tbody:nth-child(2) > tr' // na
+
+// const TIME_TH = 'tbody:nth-child(1) > tr > th'
+// const DASHBOARD_TR = 'table > tbody:nth-child(2) > tr:not(tr:first-child)' // portal
+
+function getTargetCells(withZone = false) {
+  // ------------- check code below
+  return withZone
+    ? [...document.querySelectorAll(DASHBOARD_TR)].map((row) => [...row.children].slice(1, 5))
+    : [...document.querySelectorAll(DASHBOARD_TR)].map((row) => [...row.children].slice(2, 5))
+  // ------------- check code above
+}
+
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      resolve(document.querySelector(selector))
+    }
+    const observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector))
+        observer.disconnect()
+      }
+    })
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    })
+  })
+}
+
+function test() {
+  getTargetCells()
+  const rows = getTargetCells()
+  const rowsWithTitle = getTargetCells(true)
+  console.log(rows)
+  console.log(rowsWithTitle)
+  console.log([...rowsWithTitle[0][0].classList])
+  console.log([...rowsWithTitle[1][0].classList])
+  console.log([...rowsWithTitle[2][0].classList])
+  console.log([...rowsWithTitle[3][0].classList])
+  console.log([...rowsWithTitle[4][0].classList])
+  console.log([...rowsWithTitle[5][0].classList])
+  console.log([...rowsWithTitle[6][0].classList])
+}
+
+waitForElm(TIME_TH).then(() => test())
---
+++ dev/bob2.user.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name         bob2
+// @namespace    https://github.com/ethanhong/amzn-tools/tree/main/release
+// @version      1.0.0
+// @description  A better outbound dashboard
+// @author       Pei
+// @match        https://aftlite-portal.amazon.com/ojs/OrchaJSFaaSTCoreProcess/OutboundDashboard
+// @match        https://aftlite-na.amazon.com/OutboundDashboard
+// @supportURL   https://github.com/ethanhong/amzn-tools/issues
+// ==/UserScript==
+
+// na
+const TIME_TH = '#cpt_table > thead > tr > th'
+const PICKLIST_BY_STATE = '/wms/view_picklists?state='
+
+// portal
+// const TIME_TH = 'tbody:nth-child(1) > tr > th'
+// const PICKLIST_BY_STATE = '/list_picklist/view_picklists?state='
+
+const ANY_CELL_SELECTOR = 'any > cell > selector'
+
+function aTag(content, zone, state, timeFrame) {
+  const elm = document.createElement('a')
+  elm.setAttribute('href', `${PICKLIST_BY_STATE}${state}&zone=${zone}&type=&cpt=${timeFrame}`)
+  elm.setAttribute('target', '_obd')
+  elm.textContent = content
+  return elm
+}
+
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      resolve(document.querySelector(selector))
+    }
+    const observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector))
+        observer.disconnect()
+      }
+    })
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    })
+  })
+}
+
+function test() {
+  const anyCell = document.querySelector(ANY_CELL_SELECTOR)
+
+  // ------------- check code below
+  anyCell.removeChild(anyCell.firstChild)
+  anyCell.append(aTag(123, 'ambient', 'dropped', '64,89'))
+  // ------------- check code above
+}
+
+waitForElm(TIME_TH).then(() => test())
