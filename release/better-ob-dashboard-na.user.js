b6381944add4831622ac5e5c34fe7bdc868a79fd
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-11-07 13:37:08 -0800

    init commit

---
+++ release/better-ob-dashboard-na.user.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-promise-executor-return */
+// ==UserScript==
+// @name         Better Outbound Dashboard
+// @namespace    https://github.com/ethanhong/amzn-tools/tree/main/release
+// @version      1.0.1
+// @description  A better outbound dashboard
+// @author       Pei
+// @match        https://aftlite-na.amazon.com/outbound_dashboard/index
+// @updateURL    https://ethanhong.github.io/amzn-tools/release/better-ob-dashboard-na.user.js
+// @downloadURL  https://ethanhong.github.io/amzn-tools/release/better-ob-dashboard-na.user.js
+// @supportURL   https://github.com/ethanhong/amzn-tools/issues
+// ==/UserScript==
