1bab0f77ff56472e79989c20415985f4b5870686
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-11-10 14:43:44 -0800

    change version number to force user update

--- release/ultimate-order-view.user.js
+++ release/ultimate-order-view.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ultimate Order View
 // @namespace    https://github.com/ethanhong/amzn-tools/tree/main/release
-// @version      2.1.5
+// @version      2.2.1
 // @description  Show an integrated and functional table in order view page
 // @author       Pei
 // @match        https://aftlite-na.amazon.com/wms/view_order*
