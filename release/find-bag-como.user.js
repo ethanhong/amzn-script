4b99f60eaa0401b3b32a51ab54d987aa44d169d6
ethanhong <20248019+ethanhong@users.noreply.github.com>
2022-11-14 13:38:27 -0800

    update version number

--- release/find-bag-como.user.js
+++ release/find-bag-como.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Find Bags [como]
 // @namespace    https://github.com/ethanhong/amzn-tools/tree/main/release
-// @version      2.0.6
+// @version      2.1.0
 // @description  Return stage locations for given scannable codes
 // @author       Pei
 // @match        https://como-operations-dashboard-iad.iad.proxy.amazon.com/store/*
